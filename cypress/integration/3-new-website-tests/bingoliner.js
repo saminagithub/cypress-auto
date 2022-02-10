3525b15a6ae0d9666074fec23b8349e454b96c98
saminagithub <Sabimahomed@gmail.com>
2022-02-10 08:06:56 +0200

    adding updated test bingoliner to external repo

--- cypress/integration/3-new-website-tests/bingoliner.js
+++ cypress/integration/3-new-website-tests/bingoliner.js
@@ -4,13 +4,11 @@ it('Log in test',function()
 {
 cy.visit("https://m.bingoliner.com/register/");
 cy.wait(2000);
-cy.get('.already-registered').click();
-cy.wait(2000);
-cy.get('.new-here').click();
-cy.wait(2000);
-//cy.get('#.sc-crHmcD > :nth-child(1)').type("samina");
+//cy.get('.already-registered').click();
+//cy.wait(2000);
+//cy.get('.new-here').click();
 cy.get('.sc-crHmcD > :nth-child(1) > :nth-child(1) > .MuiFormControl-root').type('samina');
-cy.wait(2000);
+//cy.wait(2000);
 cy.get(':nth-child(2) > .item > .MuiFormControl-root').type('sammytest@fun.com');
 cy.wait(2000);
 cy.get(':nth-child(8) > .item').click();
@@ -21,9 +19,19 @@ cy.get('.sc-dlVxhl > .MuiFormControl-root > .MuiFormGroup-root > .MuiFormControl
 cy.wait(2000);
 cy.get('.style-one').click();
 cy.wait(2000);
+cy.get('#fc_widget').click();
+cy.wait(2000);
 cy.get('.MuiButton-label').contains('REGISTER');
 cy.wait(2000);
 cy.get('.logo').should('be.visible');
+cy.wait(2000);
+cy.get(':nth-child(5) > [style="flex: 1 1 0%;"] > .sc-iqseJM > .MuiFormControl-root > .MuiInputBase-root > .MuiSelect-root').click();
+cy.wait(2000);
+cy.get('.MuiList-root > [tabindex="0"]').click();
+cy.wait(2000);
+cy.get('#fc_widget').dblclick();
+//cy.wait(2000);
+//cy.get('#fc_widget').click();
 });
 
 });
